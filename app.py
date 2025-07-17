df67160b02d3d318eb34606c54c35c653f205f5e
Katia <ruthnoldin91@gmail.com>
2025-07-17 19:49:57 -0300

    commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "¡Hola desde la Unida! universidad"
+    return "¡Hola desde la Unida!"
 if __name__ == '__main__':
     app.run(debug=True)
 
